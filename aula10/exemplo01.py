56762cd07e468e029fa13afdebb3cc65a119eeae
CdnCarvalho <claudenirfer@gmail.com>
2026-05-05 19:26:26 -0300

    inicializa o repositório aula 10 - Try

---
+++ aula10/exemplo01.py
@@ -0,0 +1,14 @@
+# Cálculo de produtividade
+# ---------------------------
+print('=== Cálculo de Produtividade ===')
+try:
+    total_produzido = float(input('Valor total da venda: '))
+    funcionarios = int(input('Total de Funcionários: '))
+    
+    media_por_funcionario = total_produzido / funcionarios
+    print(f'Média por funcinário: {media_por_funcionario:.2f}')
+except ValueError:
+    print('Informe um número.')
+except ZeroDivisionError:
+    print('Funcionário não pode ser Zero.')
+
---
+++ aula10/exemplo04.py
@@ -0,0 +1,17 @@
+print('=== Cálculo de Produtividade ===')
+
+try:
+    total_produzido = float(input('Valor total da venda: '))
+    funcionarios = int(input('Total de Funcionários: '))    
+    media_por_funcionario = total_produzido / funcionarios
+
+except (ValueError, TypeError):
+    print('O valor precisa ser numérico')
+except ZeroDivisionError:
+    print('Funcionário não pode ser Zero.')
+else:
+    print(f'Média por funcinário: {media_por_funcionario:.2f}')
+    
+# Executa sempre. Com erro ou não, o bloco finally sempre irá executar
+finally:
+    print('Programa encerrado!')
