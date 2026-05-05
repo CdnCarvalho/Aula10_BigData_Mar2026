065212e5cb4ed4024cfa85bfe10c7e72bb4b050c
CdnCarvalho <claudenirfer@gmail.com>
2026-05-05 20:25:11 -0300

    implementa Exception - erro geral

---
+++ aula10/exemplo05.py
@@ -0,0 +1,17 @@
+print('=== Cálculo de Produtividade ===')
+
+try:
+    total_produzido = float(input('Valor total da venda: '))
+    funcionarios = int(input('Total de Funcionários: '))    
+    media_por_funcionario = total_produzido / funcionarios
+
+except Exception as e:
+    print(f'Ops! Erro nos valores de entrada: {e}')
+except KeyboardInterrupt:
+    print('Operação cancelada pelo usuário')
+else:
+    print(f'Média por funcinário: {media_por_funcionario:.2f}')
+
+# Executa sempre. Com erro ou não, o bloco finally sempre irá executar
+finally:
+    print('Programa encerrado!')
